8490f7316cfb7c3cc5b004dc2f01c568bfa67af4
Varinder Singh <varindersingh@calvisitor-10-105-130-184.calvisitor.1918.berkeley.edu>
2019-11-21 15:43:16 -0800

    authentication added

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
-from flask import Flask,request, jsonify
-from flask_sqlalchemy import SQLAlchemy 
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
-
+import jwt
+import datetime
+from functools import wraps
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisissecret'
@@ -71,7 +73,7 @@ def create_user():
     return jsonify({'message': 'New user created!'})
 
 
-
+#route to update user info - change admin role
 @app.route('/user/<public_id>', methods=['PUT'])
 def promote_user(public_id):
     user = User.query.filter_by(public_id=public_id).first()
@@ -83,10 +85,46 @@ def promote_user(public_id):
     db.session.commit()
     return jsonify({'message': 'user has been promoted'})
 
-@app.route('/user/<public_id>', methods=['DELETE'])
-def delete_user():
-    return ''
 
+
+#route to delete a user from table
+@app.route('/user/<public_id>', methods=['DELETE'])
+def delete_user(public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message': 'user not found'})
+
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message': 'user has been deleted!!!!'})
+
+#add authentication
+@app.route('/login')
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify1', 401, {'WWW-Authenticate' : 'Basic realm="Login required!1"'})
+
+    user = User.query.filter_by(name=auth.username).first()
+
+    if not user:
+        return make_response('Could not verify username', 401, {'WWW-Authenticate' : 'Basic realm="Login username required!"'})
+
+    print(user.password)
+    print(user.name)
+    print(auth.password)
+    print(check_password_hash(user.password, auth.password))
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token' : token.decode('UTF-8')})
+    #{
+    #   "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwdWJsaWNfaWQiOiIxOTZjMzRmNi00Yjc4LTRmMjAtODQxZC1iZDcxMzcyYjZjNTIiLCJleHAiOjE1NzQzODE1MDN9.1XnGYknYd2wlFKsn2PaZ_n7ltE8szog-FUgTddtYiwY"
+    # }
+    return make_response('Could not verify3', 401, {'WWW-Authenticate' : 'Basic realm="Login required!3"'})
 
 if __name__ == '__main__':
     app.run(debug=True)
