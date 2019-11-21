6ff665e0394e4a4e24b3adb3416703e969eca195
Varinder Singh <varindersingh@calvisitor-10-105-130-184.calvisitor.1918.berkeley.edu>
2019-11-21 13:47:02 -0800

    get users API route

--- api.py
+++ api.py
@@ -10,7 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jwtAuth.db'
 
 db = SQLAlchemy(app)
 
-
+#create db schema
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     public_id = db.Column(db.String(50), unique=True)
@@ -18,32 +18,45 @@ class User(db.Model):
     password = db.Column(db.String(500))
     admin = db.Column(db.Boolean)
 
-
+#API route to get all users. 
 @app.route('/user', methods=['GET'])
 def get_all_users():
+    users = User.query.all()
+
+    output = []
+
+    for user in users:
+        user_data={}
+        user_data['public_id'] = user.public_id
+        user_data['name'] = user.name
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+        output.append(user_data)
+    return jsonify({'users': output})
+
+#API route to get one users by public_id. 
+@app.route('/user/<public_id>', methods=['GET'])
+def get_one_user(public_id):
     return ''
 
 
-@app.route('/user/<user_id>', methods=['GET'])
-def get_one_user():
-    return ''
-
+#API route to create a user by JSON. 
 @app.route('/user', methods=['POST'])
 def create_user():
     data = request.get_json()
     print (data)
     hashed_password = generate_password_hash(data['password'], method='sha256')
-    # hashed_password = generate_password_hash(data['password'], method='sha256')
+    
     new_user = User(public_id=str(uuid.uuid4()), name = data['name'], password=hashed_password, admin=False)
     db.session.add(new_user)
     db.session.commit()
     return jsonify({'message': 'New user created!'})
 
-@app.route('/user/<user_id>', methods=['PUT'])
+@app.route('/user/<public_id>', methods=['PUT'])
 def promote_user():
     return ''
 
-@app.route('/user/<user_id>', methods=['DELETE'])
+@app.route('/user/<public_id>', methods=['DELETE'])
 def delete_user():
     return ''
 
